aaf756ad04b60c8c86bc558a5d52496df7797de5
Ismail Muhammed <charmming4u@gmail.com>
2019-03-02 15:04:27 +0100

    My first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * An App to list Task desinged with React Native App
+ * https://github.com/charmming/todolist
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import Main from './app/components/Main';
+
+
+export default class App extends Component{
+  render() {
+    return (
+        <Main />
+    );
+  }
+}
